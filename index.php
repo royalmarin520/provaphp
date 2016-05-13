8bd901b4ad5d5103cceaafbe22a81e5bc683ce9b
royalmarin520 <dennis.caroli194@outlook.com>
2016-05-13 11:56:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!HTML DOCUMENT>
 <html>
 <body>
-<h1>Questa è una prova, per la dimostrazione TeP</h1>
+<h1>Questa e' una prova, per la dimostrazione TeP</h1>
 <a href="https://github.com/royalmarin520">Contatta lo sviluppatore per ulteriori informazioni</a>
 </body>
 </html>
