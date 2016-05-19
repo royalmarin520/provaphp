e70df0448c6a600187af57eeb8cc471c89703e6a
royalmarin520 <dennis.caroli194@outlook.com>
2016-05-19 12:49:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!HTML DOCUMENT>
 <html>
 <body>
-<h1>Questa e' una prova, per la dimostrazione TeP ab</h1>
+<h1>Dimostrazione test</h1>
 <a href="https://github.com/royalmarin520">Contatta lo sviluppatore per ulteriori informazioni</a>
 </body>
 </html>
